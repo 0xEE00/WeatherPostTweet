9b8ced8180d0ba9c8b5a54de91af3a253e942b9d
Drazen Blanusa <blanusa@outlook.com>
2017-10-06 10:17:06 +0200

    Test 1

--- WeatherPostTweet/StartupTask.cs
+++ WeatherPostTweet/StartupTask.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Net.Http;
 using Windows.ApplicationModel.Background;
+using System.Xml;
+using System.Net;
+using System.Threading.Tasks;
+using System.IO;
 
 // The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
 
@@ -11,6 +15,8 @@ namespace WeatherPostTweet
 {
     public sealed class StartupTask : IBackgroundTask
     {
+        string WeatherInfo = "";
+
         public void Run(IBackgroundTaskInstance taskInstance)
         {
             // 
@@ -20,6 +26,25 @@ namespace WeatherPostTweet
             // from closing prematurely by using BackgroundTaskDeferral as
             // described in http://aka.ms/backgroundtaskdeferral
             //
+            MakeWebRequest(@"http://www.hidmet.gov.rs/ciril/osmotreni/kosutnjak.xml");
+        }
+
+        async public void MakeWebRequest(string url)
+        {
+            //var webRequest = WebRequest.Create(@"http://www.hidmet.gov.rs/ciril/osmotreni/kosutnjak.xml");
+
+            //using (var response1 = await webRequest.GetResponseAsync())
+            //using (var content = response1.GetResponseStream())
+            //using (var reader = new StreamReader(content))
+            //{
+            //    var strContent = reader.ReadToEnd();
+            //}
+
+
+
+            HttpClient http = new HttpClient();
+            HttpResponseMessage response = await http.GetAsync(new Uri(url));
+            WeatherInfo = await response.Content.ReadAsStringAsync();
         }
     }
 }
